3c3c5ad1806a6990f89d1e308fdd8d65444e9e9f
reDpz <sv_redpz@outlook.com>
2025-01-14 22:17:08 +0000

    init

---
+++ list_comp_macro/target/debug/deps/list_comp_macro-defa1b71fc087442.d
@@ -0,0 +1,9 @@
+/home/fart/nixprojects/proc_test/list_comp_macro/target/debug/deps/liblist_comp_macro-defa1b71fc087442.rmeta: src/lib.rs Cargo.toml
+
+/home/fart/nixprojects/proc_test/list_comp_macro/target/debug/deps/list_comp_macro-defa1b71fc087442.d: src/lib.rs Cargo.toml
+
+src/lib.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=--no-deps__CLIPPY_HACKERY__
+# env-dep:CLIPPY_CONF_DIR
